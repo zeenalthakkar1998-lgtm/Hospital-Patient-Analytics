3129e890cc3360b651a819afca0699291e8aefe7
zeenalthakkar1998-lgtm <zeenalthakkar1998@gmail.com>
2026-07-20 18:34:35 +0530

    Complete SQL module 1:Basic SELECT queries

---
+++ sql/01_basic_queries.sql
@@ -0,0 +1,11 @@
+--count total patient records
+SELECT COUNT(*) FROM healthcare_dataset
+
+-- Display patient names
+SELECT Name FROM healthcare_dataset;
+
+-- Display patient names and ages
+SELECT Name, Age FROM healthcare_dataset;
+
+-- Display all columns
+SELECT * FROM healthcare_dataset;
